b79e9167b89fdd21851e59dd6f2bb1822b40d304
alvimcode <atset.tecnologia@gmail.com>
2022-10-01 17:57:20 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/alvimcode/cursomc/services/CategoriaService.java
+++ src/main/java/com/alvimcode/cursomc/services/CategoriaService.java
@@ -20,4 +20,11 @@ public class CategoriaService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException( 
 		 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName())); 
 		}
+	
+	//criando o metodo insert do CategoriaResource
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+
 }
