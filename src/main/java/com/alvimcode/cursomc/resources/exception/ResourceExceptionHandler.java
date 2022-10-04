bcd60c3fbfbc0ae949079dce088ec3ab1c38836c
alvimcode <atset.tecnologia@gmail.com>
2022-10-04 10:57:45 -0300

    personalizando o retorno da validação

--- src/main/java/com/alvimcode/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/alvimcode/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -2,6 +2,8 @@ package com.alvimcode.cursomc.resources.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.client.HttpServerErrorException;
@@ -22,8 +24,23 @@ public class ResourceExceptionHandler {
 	@ExceptionHandler(DataIntegrityException.class)
 	public ResponseEntity<StandardError> DataIntegrity(DataIntegrityException e, HttpServerErrorException request) {
 
-		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(),
+				System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e,
+			HttpServerErrorException request) {
+
+		ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(),"Errors",
+				System.currentTimeMillis());
+
+		for (FieldError x : e.getBindingResult().getFieldErrors()) {
+			err.addError(x.getField(), x.getDefaultMessage());
+		}
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 
 }
