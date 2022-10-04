2c9f1810081fb435d59682ccb183c752ea90d271
alvimcode <atset.tecnologia@gmail.com>
2022-10-04 10:37:46 -0300

    Validacao sintatica com Bean validation

--- src/main/java/com/alvimcode/cursomc/services/CategoriaService.java
+++ src/main/java/com/alvimcode/cursomc/services/CategoriaService.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.alvimcode.cursomc.domain.Categoria;
+import com.alvimcode.cursomc.dto.CategoriaDTO;
 import com.alvimcode.cursomc.repositories.CategoriaRepository;
 import com.alvimcode.cursomc.services.exceptions.DataIntegrityException;
 import com.alvimcode.cursomc.services.exceptions.ObjectNotFoundException;
@@ -55,6 +56,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 
 }
