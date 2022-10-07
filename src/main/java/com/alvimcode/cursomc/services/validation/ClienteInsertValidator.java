510d1abb360a963a5ba0ac573469fbebf46a987e
alvimcode <atset.tecnologia@gmail.com>
2022-10-06 22:58:49 -0300

    Validação customizada: Email nao repetido na insercao de clientes

--- src/main/java/com/alvimcode/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/alvimcode/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,21 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.alvimcode.cursomc.domain.Cliente;
 import com.alvimcode.cursomc.dto.ClienteNewDTO;
 import com.alvimcode.cursomc.enums.TipoCliente;
+import com.alvimcode.cursomc.repositories.ClienteRepository;
 import com.alvimcode.cursomc.resources.exception.FieldMessage;
 import com.alvimcode.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -23,10 +32,14 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CPF Invalido"));
 		}
-		
 
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ Invalido"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "email ja existe"));
 		}
 
 		for (FieldMessage e : list) {
