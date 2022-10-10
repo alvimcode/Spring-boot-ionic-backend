5f3278d00350a01404f94d7167788f1e15d3f0e4
alvimcode <atset.tecnologia@gmail.com>
2022-10-10 20:03:34 -0300

    Criando profile de teste

---
+++ src/main/java/com/alvimcode/cursomc/config/TestConfig.java
@@ -0,0 +1,26 @@
+package com.alvimcode.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.alvimcode.cursomc.services.DBService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	
+	@Bean
+	public boolean instantiateTestDatabase() throws ParseException {
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+
+}
