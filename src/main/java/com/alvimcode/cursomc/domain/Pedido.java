a80a1377a21e92dabf8f957b1efbf862f4f92f6f
alvimcode <atset.tecnologia@gmail.com>
2022-10-15 12:03:08 -0300

    Implementando toString de pedido

--- src/main/java/com/alvimcode/cursomc/domain/Pedido.java
+++ src/main/java/com/alvimcode/cursomc/domain/Pedido.java
@@ -1,8 +1,11 @@
 package com.alvimcode.cursomc.domain;
 
 import java.io.Serializable;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.Locale;
 import java.util.Objects;
 import java.util.Set;
 
@@ -53,7 +56,7 @@ public class Pedido implements Serializable {
 		this.cliente = cliente;
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
-	
+
 	public double getValorTotalPedido() {
 		double soma = 0.0;
 		for (ItemPedido ip : itens) {
@@ -125,6 +128,33 @@ public class Pedido implements Serializable {
 			return false;
 		Pedido other = (Pedido) obj;
 		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		//formata dinheiro
+		NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+		
+		//formata data
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss");
+		
+		StringBuilder builder = new StringBuilder();
+		builder.append("Pedido nome: ");
+		builder.append(getId());
+		builder.append(", Intante");
+		builder.append(sdf.format(getInstante()));
+		builder.append(", cliente: ");
+		builder.append(getCliente().getNome());
+		builder.append(", Situação do pagamento");
+		builder.append(getPagamento().getEstado().getDescricao());
+		builder.append("\nDetalhe:\n");
+		for (ItemPedido ip : getItens()) {
+			builder.append(ip.toString());
+		}
+		builder.append("Valor total: ");
+		builder.append(nf.format(getValorTotalPedido()));
+
+		return builder.toString();
 	}
 
 }
--- src/main/java/com/alvimcode/cursomc/services/PedidoService.java
+++ src/main/java/com/alvimcode/cursomc/services/PedidoService.java
@@ -66,6 +66,7 @@ public class PedidoService {
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
 		//emailService.sendOrderConfirmationEmail(obj);
+		System.out.println(obj);
 		return obj;
 	}
 
